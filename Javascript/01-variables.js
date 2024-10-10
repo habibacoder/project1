b930be1b9054f6f2414d5bf97a673a6af5c0b213
habibacoder <habibaparvez973@gmail.com>
2024-10-10 17:53:18 +0530

    Add files via upload

---
+++ Javascript/01-variables.js
@@ -0,0 +1,13 @@
+let  accountId  = 12345
+let accountemail = "hbia2gmail.com"
+var accountPassword = "4567"
+accountcity = "jaipur"
+let accountState;
+
+accountId = 2
+
+accountemail = "mary@gmail.com"
+  console.log(accountId);
+
+  console.table([accountId,accountemail,accountPassword,accountcity,accountState])
+  
---
+++ Javascript/02-dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code  as a newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3
+    +
+    3)   //code readibility should be high
+
+console.log("habiba")
+
+ let name = "hbi"
+ let age = 22
+  let isloggedin = false
+
+   //data types
+
+   //number 
+   //bigint = lmbi value k lye use hota hain
+   //string = " "
+   //boolean = true/false
+    //null = standalone value
+    //undefined = vlue is not defined
+     //Symbol = unique
+    //object 
+    //type of
+
+      console.log(typeof age)
